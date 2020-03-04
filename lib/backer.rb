a8eeac69699ed6559183c9aa299f1c89c4548399
Willis Aybar <willisaybar@gmail.com>
2020-03-04 01:08:09 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+
+  attr_accessor :project, :backers, :name
+  attr_reader :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_project = []
+  end
+
+  def back_project()
+    @backed_project << @project
+  end
+
+  # Once both classes have their attributes and readers set up, write a method on the Backer
+  # class called back_project() that takes in a Project instance and adds the project to its @backed_projects attribute.
+  #
+
+
+end
